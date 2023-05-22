2ef1a6e2c902eda0814b87390cdfc68352ad0d6b
BorozanM <76406413+BorozanM@users.noreply.github.com>
2023-05-22 17:31:06 +0200

    dodata komponenta navbar

---
+++ src/komponente/Navbar.jsx
@@ -0,0 +1,16 @@
+
+import {Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className="topnav">
+        <Link to="/">Pocetna</Link>
+        <Link to="/kontakt">Kontakt</Link>
+        
+
+
+    </div>
+  );
+}
+
+export default Navbar;
