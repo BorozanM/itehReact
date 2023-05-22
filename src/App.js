4e9798634abe29d60f7eba65595c1b32941cfd28
BorozanM <76406413+BorozanM@users.noreply.github.com>
2023-05-22 19:11:43 +0200

    kreirane 2 stranice, kao i osnovne rute

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import Navbar from './komponente/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Pocetna from './komponente/Pocetna';
+import Kontakt from './komponente/Kontakt';
 
 function App() {
   return (
     <div >
       <BrowserRouter className="App">
-    <Navbar ></Navbar>
-   
+    <Navbar ></Navbar> {/* ne stavljamo rutu jer hocemo da se ovo svugde prikazuje */}
+   <Routes>
+<Route path="/"element={<Pocetna></Pocetna>}></Route>
+<Route path="/kontakt"element={<Kontakt></Kontakt>}></Route>
+
+   </Routes>
   </BrowserRouter>
     </div>
   );
---
+++ src/komponente/Pocetna.jsx
@@ -0,0 +1,12 @@
+
+function Pocetna() {
+  return (
+    <div >
+        
+<h1>Ovo je pocetna</h1>
+
+    </div>
+  );
+}
+
+export default Pocetna;
