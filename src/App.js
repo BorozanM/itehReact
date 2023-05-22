4976d4fe14f0e30119050f0d8cd7f6bb468a5e71
BorozanM <76406413+BorozanM@users.noreply.github.com>
2023-05-22 19:58:16 +0200

    dodat futer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './komponente/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Pocetna from './komponente/Pocetna';
 import Kontakt from './komponente/Kontakt';
+import Footer from './komponente/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
 <Route path="/kontakt"element={<Kontakt></Kontakt>}></Route>
 
    </Routes>
+   <Footer></Footer>
   </BrowserRouter>
     </div>
   );
