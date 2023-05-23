b69a2a0305cb0e75624b590fdf311a8a63be3f1e
BorozanM <76406413+BorozanM@users.noreply.github.com>
2023-05-23 14:45:10 +0200

    dodat sadrzaj na pocetnu stranicu

--- src/komponente/Kartica.jsx
+++ src/komponente/Kartica.jsx
@@ -1,23 +1,23 @@
 
-function Kartica() {
+function Kartica({laptop}) {
     return (
       <div className="card" >
               <div className="card-header">
-                    <img src="" alt="" className="card-img-top" />{/* ceo ovaj div je slika! */}
+                    <img src={laptop.slika} alt="" className="card-img-top" />{/* ceo ovaj div je slika! */}
               </div>
               <div className="card-body">{/* podaci o toj kartici na kojoj ce se predstavljati */}
-                <h3 className="product-name">{}</h3>
+                <h3 className="product-name">{laptop.model}</h3>
                
                 <h5 className='karakteristike'>
                     
-                    Ekran: {} <br />
-                    Cena: {} <br />        
-                    Baterija: {} <br />     
-                    Boja: {} <br />        
-
+                    Ekran: {laptop.ekran} <br />
+                    Cena: {laptop.cena} <br />        
+                    Baterija: {laptop.baterija} <br />     
+                    Boja: {laptop.boja} <br />        
+                    
                 </h5>
               </div>
-            <button className="btnCompare" ></button>
+            <button className="btnCompare" >Uporedi</button>
       </div>
   
   
